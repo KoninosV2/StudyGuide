967f952730372031f3f0f3718026b9c47d4ffa09
KoninosV2 <koninos95@gmail.com>
2021-06-21 03:20:48 +0300

    settings fix?

--- includes/dbcon.php
+++ includes/dbcon.php
@@ -1,17 +1,14 @@
 <?php
 session_start();
 $host = 'localhost';
-include("/dbcon/db_settings.php");  
+include("../dbcon/db_settings.php");
 try {
     $dsn = "mysql:host=$host;dbname=$dbname";
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-
-unset ($dbname, $user, $pass); 
