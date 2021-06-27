85508f88d7d57e36c952ebb1d07b921718c3daee
KoninosV2 <koninos95@gmail.com>
2021-06-27 16:34:12 +0300

    remove book count from public home page

--- index.php
+++ index.php
@@ -9,12 +9,6 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $number_of_lessons = $stmt->rowCount();
 
-// Πλήθος Συγγραμμάτων
-$sql = "SELECT * FROM book";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$number_of_books = $stmt->rowCount();
-
 // Πλήθος Καθηγητών
 $sql = "SELECT * FROM teacher";
 $stmt = $pdo->prepare($sql);
@@ -39,22 +33,6 @@ $number_of_teachers = $stmt->rowCount();
             </div>
             <div class="col-auto">
               <i class="fas fa-bookmark fa-3x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </a>
-    </div>
-
-    <div class="col-xl-3 col-md-6 mb-4">
-      <a role="button" href="books.php" class="btn-outline-light card border-left-success shadow py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Συγγραμματα</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $number_of_books; ?></div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-book fa-3x text-gray-300"></i>
             </div>
           </div>
         </div>
